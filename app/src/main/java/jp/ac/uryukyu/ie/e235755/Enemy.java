fa22e6c3e13f79148b30620435707c3406689e3c
Ryuichi Nakahodo <e235755@ie.u-ryukyu.ac.jp>
2023-11-12 11:19:52 +0900

    Second commit

--- app/src/main/java/jp/ac/uryukyu/ie/e235755/Enemy.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e235755/Enemy.java
@@ -35,8 +35,10 @@ public class Enemy {
      */
     public void attack(Hero hero){
         int damage = (int)(Math.random() * attack);
-        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.name, damage);
-        hero.wounded(damage);
+        if (dead == false){
+            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.name, damage);
+            hero.wounded(damage);
+        }
     }
 
     /**
